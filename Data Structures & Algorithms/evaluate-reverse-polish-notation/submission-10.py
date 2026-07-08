918dfb85c4c058bc9423091aaddafc9ed83caef2
Cretu Calin <cretucalin06@yahoo.ro>
2026-07-08 16:00:12 +0300

    Add: evaluate-reverse-polish-notation - submission-10

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-10.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isOperation(self, s: str) -> bool:
+        return s in ["+", "-", "*", "/"]
+ 
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for i in range(len(tokens)):
+            if not self.isOperation(tokens[i]):
+                stack.append(int(tokens[i]))
+            else:
+                number1 = stack.pop()
+                number2 = stack.pop()
+
+
+                operation = tokens[i]
+                if operation == "+":
+                    stack.append(number2 + number1)
+                if operation == "-":
+                    stack.append(number2 - number1)
+                if operation == "*":
+                    stack.append(number2 * number1)
+                if operation == "/":
+                    stack.append(int(number2 / number1))
+        return stack.pop()
