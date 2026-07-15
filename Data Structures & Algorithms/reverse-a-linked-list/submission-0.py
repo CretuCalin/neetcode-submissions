4d3cdf3a04c100435ff97e677c22822cb3d90761
Cretu Calin <cretucalin06@yahoo.ro>
2026-07-15 13:02:40 +0300

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        stack = []
+        curr = head
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+        
+        curr = head
+        while stack:
+            val = stack.pop()
+            curr.val = val
+            curr = curr.next
+
+        return head
